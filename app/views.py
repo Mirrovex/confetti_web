9667106aeaaa3216f2fa0d5cc4e956727421d580
Mirrovex <mirrovex@wp.pl>
2024-05-04 16:00:02 +0200

    confetti

--- app/views.py
+++ app/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Strona główna',
-        'content': 'Hello World 2',
     }
     return render(request, 'index.html', context)
